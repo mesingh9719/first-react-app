d6c9a4ae8c7f47820f9e99c36a1461059e1f02bb
surinder singh <mesingh9719@gmail.com>
2024-01-08 23:28:07 +0530

    props child to parent and parent to sibling

--- src/components/users-assets/User.js
+++ src/components/users-assets/User.js
@@ -1,43 +1,21 @@
-import Thumb from "./Thumb";
 import UsersData from "../data/UsersData";
-import PlayButton from "../atoms/PlayButton";
 import {useState} from "react";
+import AddUser from "./AddUser";
+import UsersList from "./UsersList";
 function User() {
     const [users,setUsers] = useState(UsersData)
+    function addUsers(user) {
+        setUsers(prevUsers => [
+            ...prevUsers,
+            { ...user, id: prevUsers.length + 1 }
+        ]);
+    }
     return (<>
         <div>
             <div>
-                <button className='btn' onClick={(e)=>{
-                    e.stopPropagation();
-                    const newUserRecord = [...users,{
-                        id: users.length+1,
-                        name: "xyz singh",
-                        mobile: 8283089269,
-                        imageLink: "https://picsum.photos/200/300?random="+users.length+1,
-                        age: 28,
-                        verified: false
-                    }]
-                setUsers(newUserRecord)
-                }}>Add Videos</button>
+                <AddUser newUser={addUsers}></AddUser>
             </div>
-            <div className="d-flex flex-wrap justify-content-space-between">
-                {
-                    users.map(user =>
-                        <Thumb
-                            key={user.id}
-                            name={user.name}
-                            imageLink={user.imageLink}
-                            mobile={user.mobile}
-                            age={user.age}
-                            verified={user.verified}
-                            id={user.id}
-                        >
-                            <PlayButton msg="Song Playing" onPlay={() => { console.log('Playing 1') }} onPause={() => { console.log('Pause 1') }}>Play</PlayButton>
-                        </Thumb>
-                    )
-                }
-            </div>
-
+            <UsersList users={users}></UsersList>
         </div>
     </>)
 }

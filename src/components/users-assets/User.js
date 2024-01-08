7b94154d530b5493909a4c8ac09d6d49f6227ebb
surinder singh <mesingh9719@gmail.com>
2024-01-08 21:58:06 +0530

    props and state

--- src/components/users-assets/User.js
+++ src/components/users-assets/User.js
@@ -1,13 +1,28 @@
 import Thumb from "./Thumb";
 import UsersData from "../data/UsersData";
 import PlayButton from "../atoms/PlayButton";
+import {useState} from "react";
 function User() {
-
+    const [users,setUsers] = useState(UsersData)
     return (<>
         <div>
+            <div>
+                <button className='btn' onClick={(e)=>{
+                    e.stopPropagation();
+                    const newUserRecord = [...users,{
+                        id: users.length+1,
+                        name: "xyz singh",
+                        mobile: 8283089269,
+                        imageLink: "https://picsum.photos/200/300?random="+users.length+1,
+                        age: 28,
+                        verified: false
+                    }]
+                setUsers(newUserRecord)
+                }}>Add Videos</button>
+            </div>
             <div className="d-flex flex-wrap justify-content-space-between">
                 {
-                    UsersData.map(user =>
+                    users.map(user =>
                         <Thumb
                             key={user.id}
                             name={user.name}

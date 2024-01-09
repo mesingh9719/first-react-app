a3c7a3132d16553f2ce5e9418cd62448d28ca026
surinder singh <mesingh9719@gmail.com>
2024-01-09 17:35:41 +0530

    add use effect

--- src/components/users-assets/Thumb.js
+++ src/components/users-assets/Thumb.js
@@ -1,16 +1,17 @@
 import '../css/ThumbStyle.css';
 import ImageFig from '../atoms/ImageFig';
-function Thumb({ name, mobile, imageLink, age, verified, children }) {
+function Thumb({ name, mobile, imageLink, age, verified,userId, children,deleteUser,editUser }) {
+    const playButton = children;
     return (<>
         <div className='card b-r-10 box-shadow-sm font-b-600 text-capitalized'>
             <div>
-            <ImageFig imageTitle={name}>
-            <img className='object-fix-cover' src={imageLink} alt='thumb' />
-            </ImageFig>
+                <ImageFig imageTitle={name}>
+                    <img className='object-fix-cover' src={imageLink} alt='thumb'/>
+                </ImageFig>
             </div>
             <div className='p-10'>
                 <div className='d-flex justify-content-between align-items-center'>
-                <div className='font-12'>
+                    <div className='font-12'>
                         {age ?? "Age"}
                     </div>
                     <div className='font-12'>
@@ -18,14 +19,29 @@ function Thumb({ name, mobile, imageLink, age, verified, children }) {
                     </div>
                 </div>
                 <div className='d-flex justify-content-end align-items-center '>
-                    
+
                     <div className='active font-10'>
                         {verified ? 'Enabled' : 'Disabled'}
                     </div>
                 </div>
             </div>
-            <div>
-                {children}
+
+            <div className='d-flex justify-content-space-between'>
+                <div className='w-100 text-left'>
+                    {playButton}
+                </div>
+                <div>
+                    <button className='btn btn-danger' onClick={() => {
+                        deleteUser(userId)
+                    }}>X
+                    </button>
+                </div>
+                <div>
+                    <button className='btn btn-danger' onClick={() => {
+                        editUser(userId)
+                    }}>Edit
+                    </button>
+                </div>
             </div>
         </div>
     </>);
--- src/components/users-assets/User.js
+++ src/components/users-assets/User.js
@@ -4,18 +4,40 @@ import AddUser from "./AddUser";
 import UsersList from "./UsersList";
 function User() {
     const [users,setUsers] = useState(UsersData)
+    const [editAbleUser,setEditableUser] = useState(null);
     function addUsers(user) {
         setUsers(prevUsers => [
             ...prevUsers,
             { ...user, id: prevUsers.length + 1 }
         ]);
     }
+
+    function deleteUser(userId){
+        console.log(userId)
+        setUsers(users.filter(user=>user.id!==userId))
+    }
+
+    function editUser(userId){
+        console.log(users.find(user=>user.id === userId))
+        console.log(userId)
+        setEditableUser(users.find(user=>user.id === userId))
+    }
+
+    function updateUser(user){
+        console.log("changed info")
+        const index = users.findIndex(usr=>usr.id===user.id)
+        const newUsersData = [...users]
+        newUsersData.splice(index,1,user);
+        setUsers(newUsersData);
+        console.log(newUsersData);
+    }
+
     return (<>
         <div>
             <div>
-                <AddUser newUser={addUsers}></AddUser>
+                <AddUser newUser={addUsers} editAbleUser={editAbleUser} updateUser={updateUser}></AddUser>
             </div>
-            <UsersList users={users}></UsersList>
+            <UsersList deleteUser={deleteUser}  users={users} editUser={editUser}></UsersList>
         </div>
     </>)
 }
